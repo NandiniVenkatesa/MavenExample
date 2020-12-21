abf94f85737166f6dc0777bfc5b070c4f2d41217
NandiniVenkatesa <Nandini.VenkatesaBabu@lntinfotech.com>
2020-12-21 15:45:46 +0530

    Maven Project

---
+++ JarDemo/src/main/java/com/lti/demo/App.java
@@ -0,0 +1,17 @@
+package com.lti.demo;
+//import com.lti.myapp.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //com.lti.myapp.App a = new com.lti.myapp.App();
+        //App a = new App();
+        //a.fun();
+    }
+}
